22f8a5330c8a533c90dce70874752ccf434ad998
PremKumarHub <ey78627@gmail.com>
2024-12-12 15:35:24 +0530

    interval

---
+++ students-xerox/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './Footer.css';
+
+const Footer = () => {
+
+
+  return (
+
+    <footer className="footer">
+    <p>&copy; 2024 Students Xerox. All rights reserved.</p>
+  </footer>
+  
+
+  
+  );
+};
+
+export default Footer;
---
+++ students-xerox/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <h1>Students Xerox</h1>
+      </div>
+      <ul className="navbar-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
