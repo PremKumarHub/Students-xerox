267bbfee4f8c30fb2e6e8ffdf0ec6ad9bb86a035
PremKumarHub <ey78627@gmail.com>
2025-01-05 17:31:36 +0530

    responsive commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         <h1>Students Xerox</h1>
       </div>
-      <ul className="navbar-links">
+      <button className="navbar-hamburger" onClick={toggleMobileMenu}>
+        &#9776;
+      </button>
+      <ul className={`navbar-links ${isMobileMenuOpen ? 'active' : ''}`}>
         <li><a href="#home">Home</a></li>
         <li><a href="#services">Services</a></li>
         <li><a href="#location">Location</a></li>
